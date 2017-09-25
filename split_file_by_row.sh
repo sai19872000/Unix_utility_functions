e6ed19369a961d21676db69320267c4bf2812331
Dr. Sai Teja Pusuluri <sai19872000@gmail.com>
2017-09-25 10:41:47 -0400

    Update split_file_by_row.sh

--- split_file_by_row.sh
+++ split_file_by_row.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 #split line in 5 parts
-tail -n +2 file.txt | split -l 5 - split_
-for file in split_*
+echo "Enter the file name without extention and press [Enter]:"
+read file
+csv=".csv"
+split="split_"
+file_name=$file$csv
+split_file=$file$split
+tail -n +2 $file_name | split -l 10000000 - $split_file
+for file in $split_file*
 do
-    head -n 1 file.txt > tmp_file
+    head -n 1 $file_name > tmp_file
     cat $file >> tmp_file
-    mv -f tmp_file $file
+    mv -f tmp_file $file$csv
+    rm $file
 done
